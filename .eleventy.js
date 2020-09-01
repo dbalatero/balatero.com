105b7f26a30558e6a69b80cd78fbb5f807eccfc5
David Balatero <dbalatero@stripe.com>
2020-09-01 10:20:52 -0400

    Add sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -6,11 +6,35 @@ function withTag(array, tag) {
   return array.filter(item => item.data.tags.includes(tag))
 };
 
+function httpUrl(path) {
+  return `https://balatero.com${path}`;
+};
+
+function valueIfEmpty(value, defaultValue) {
+  if (value === undefined || value === null || value === "") {
+    return defaultValue;
+  }
+
+  return value;
+};
+
+function dateDisplay(date, format) {
+  if (format === "toISOString") {
+    return date.toISOString();
+  }
+
+  return date;
+}
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/images");
 
-  eleventyConfig.addPlugin(pluginSass);
+  eleventyConfig.addFilter('dateDisplay', dateDisplay);
+  eleventyConfig.addFilter('httpUrl', httpUrl);
+  eleventyConfig.addFilter('valueIfEmpty', valueIfEmpty);
   eleventyConfig.addFilter('withTag', withTag);
+
+  eleventyConfig.addPlugin(pluginSass);
 
   eleventyConfig.addPlugin(syntaxHighlight, {
     alwaysWrapLineHighlights: false,
